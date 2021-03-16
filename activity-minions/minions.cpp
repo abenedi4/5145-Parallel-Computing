89b396ea02171c2ad8b9dc245861f85db1029f1b
abenedi4 <abenedi4@uncc.edu>
2021-03-15 20:00:04 -0400

    Finished assignment

--- activity-minions/minions.cpp
+++ activity-minions/minions.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
-
+#include <thread>
+#include <vector>
+//Method to say minion greeting based on x iteration
+void greeting(int x) {
+  std::cout<<"Hello! I am minion "<<x<<"\n";
+}
 
 int main (int argc, char** argv) {
   if (argc < 2) {
     std::cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
     return -1;
   }
-  //Your code goes here
-  
+  //Convert arg to int
+  int num = atoi(argv[1]);
+
+  //Create vector of threads to store minions
+  std::vector<std::thread> minions;
+
+  //Loop and create threads
+  for(int i = 0; i<num; ++i) {
+    minions.emplace_back(std::thread(greeting, (i + 1)));
+}
+
+  //Wait for threads to finish
+  for(auto& thread : minions){
+    thread.join();
+}
+
+  //Print last Overlord message
+  std::cout<<"Hello Minions! I am the Overlord!"<<"\n";
+
   return 0;
 }
